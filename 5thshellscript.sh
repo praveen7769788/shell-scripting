58dc7d6eada461f05c0a2c38ef46080d088963ae
praveen7769788 <pm7769788@gmail.com>
2024-10-23 01:11:42 +0530

    2nd and 3rd file are created

---
+++ 5thshellscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter person1"
+read persone1
+
+echo "enter another persone2"
+read persone2
+
+#persone1=$1
+#persone2=$2
+
+echo "$persone1::hello suresh"
+echo "$persone2:: hi $persone1"
+echo "$persone1:: how are you suresh"
+echo "$persone2:: i am good what about you"
+echo "$persone1:: same hear thankyou"
