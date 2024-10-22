71536d30407341fe5ee2655cf000c8c75113f2ee
praveen7769788 <pm7769788@gmail.com>
2024-10-23 00:37:00 +0530

    2nd and 3rd file are created

---
+++ 3rdshellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+persone1=ramesh
+persone2=suresh
+
+echo "$persone1::hello suresh"
+echo "$persone2:: hi $person1"
+echo "$persone1:: how are you suresh"
+echo "$persone2:: i am good what about you"
+echo "$persone1:: same hear thankyou"
