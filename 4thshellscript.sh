4183e6d2e386a427e05a314993de12c7db0a0b86
praveen7769788 <pm7769788@gmail.com>
2024-10-23 01:04:55 +0530

    2nd and 3rd file are created

---
+++ 4thshellscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+persone1=$1
+persone2=$2
+
+echo "$persone1::hello suresh"
+echo "$persone2:: hi $persone1"
+echo "$persone1:: how are you suresh"
+echo "$persone2:: i am good what about you"
+echo "$persone1:: same hear thankyou"
