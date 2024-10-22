3c387fa6b44b145382357e5863493879b8960a45
praveen7769788 <pm7769788@gmail.com>
2024-10-23 00:32:52 +0530

    2nd and 3rd file are created

---
+++ 2ndshellscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "ramesh::hello suresh"
+echo "suresh:: hi ramesh"
+echo "ramesh:: how are you suresh"
+echo "suresh:: i am good what about you"
+echo "ramesh:: same hear thankyou"
