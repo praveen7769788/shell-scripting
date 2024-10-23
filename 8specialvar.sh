3784df8cca4dc32821b5e38483119e4cd32cc6dc
praveen7769788 <pm7769788@gmail.com>
2024-10-23 14:58:18 +0530

    array practice

---
+++ 8specialvar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "all variables : $@"
+echo "number of veriable passed: $#"
+echo "scripth name: $0"
+echo "current working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "which user is running this script: $USER"
+echo "hostname: $HOSTNAME"
+echo "process id of current shell script: $$"
