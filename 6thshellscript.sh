a5ffc7b08ff79d8bedb665b8ff136a6b929b474c
praveen7769788 <pm7769788@gmail.com>
2024-10-23 14:05:25 +0530

    array practice

---
+++ 6thshellscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# arrey practice
+
+name=( "a" "b" "c" "d" )
+
+
+echo "enter veriable"
+read var1
+echo "enter another veriable"
+read var2
+
+echo "var1=$var1 and var2=$var2"
+
+echo "array variables ${name[@]}"
+echo "array index zero ${name[0]}"
+echo "array index zero ${name[1]}"
+echo "array index zero ${name[2]}"
+echo "array index zero ${name[3]}"
+echo "array index zero ${name[4]}"
